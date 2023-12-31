3645f69c63d289d86b4c4bb08c6e759ebfd5ddf0
Rahul Gavhar <138155613+rahulgavhar@users.noreply.github.com>
2023-12-31 13:56:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -71,12 +71,3 @@ var swiper = new Swiper(".review-slider", {
         },
     },
 });
-
-function loader() {
-    document.querySelector('.loader-container').classList.add('fade-out');
-}
-function fadeOut() {
-    setInterval(loader, 3000);
-}
-
-window.onload = fadeOut;
